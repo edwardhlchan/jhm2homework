3acf3ff62a6353427925a32591bcf7dcab82c990
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2025-01-21 23:50:19 +0800

    Refactor snack selection logic into a separate function

--- hw2.py
+++ hw2.py
@@ -1,14 +1,19 @@
-# Basic if structure
-a = input("Do you want some snacks? (yes/no)\n")
+
+def part2():
+    b = input("Enter your choice (ice-cream / cookies / candies):")
+    dt = {
+        'ice-cream': "Remember to wash your hands",
+        'cookies': "Can you share with your friends?",
+        'candies': "Don’t eat too much."
+    }
+    print(dt.get(b, "Invalid choice"))
+
+a = input("Do you want some snacks? (yes/no)")
 if a == 'no':
     print("Good! Let’s play games instead.")
 elif a == 'yes':
-    b = input("Enter your choice (ice-cream / cookies / candies):\n")
-    if b == 'ice-cream':
-        print("Remember to wash your hands")
-    elif b == 'cookies':
-        print("Can you share with your friends?")
-    elif b == 'candies':
-        print("Don’t eat too much.")
+    part2()
+
+
     # elif b == "candycookiechocolate":
     #     print("DANGO")
