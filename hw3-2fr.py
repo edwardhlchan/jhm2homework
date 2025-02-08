ed39b8fbb937bef6ed82bf90ad1172b45f5cb7ff
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2025-02-08 12:56:33 +0800

    ok i rmb

--- hw3-2fr.py
+++ hw3-2fr.py
@@ -1,9 +1,9 @@
 n = int(input("Input Addition Table Size smaller 10: "))
 print("Addition Table")
 print("-" * 55)
-expect_longest = len(str(n + n)) + 1
+expect_longest = len(str(n + n))
 for i in range(1, n+1):#loop 1
     for j in range(1, n+1): #loop 2
-        print("%d + %d = %d" % (i, j, i+j), end=(" "*(expect_longest-len(str(i+j))))) #print the result
+        print(f"{i} + {j} = {i+j}".rjust(expect_longest)) #print the result
     print()
 print("-" * 55)
