e543037e7ac5ac9f4f948cb8b7b1b2e892a3a61f
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2025-02-08 10:18:48 +0800

    paak maai

--- hw3.py
+++ hw3.py
@@ -1,8 +1,9 @@
 n = int(input("Input Addition Table Size smaller 10: "))
 print("Addition Table")
 print("-" * 55)
+expect_longest = len(str(n + n)) + 1
 for i in range(1, n+1):#loop 1
     for j in range(1, n+1): #loop 2
-        print("%d + %d = %d" % (i, j, i+j), end=" ") #print the result
+        print("%d + %d = %d" % (i, j, i+j), end=(" "*(expect_longest-len(str(i+j))))) #print the result
     print()
 print("-" * 55)
