e23a550e850ec4d1a7c9549099b16f048dab7fe1
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2025-02-15 10:46:23 +0800

    remove len

--- hw3-1fr.py
+++ hw3-1fr.py
@@ -4,7 +4,8 @@ print("-" * 55)
 expect_longest = len(str(n + n)) + 1
 stra = f""
 for i in range(n*n):
-    stra += f"{i//n + 1} + {i%n + 1} = {(i//n+1) + (i%n+1)}" + " "*(expect_longest - len(str((i//n+1) + (i%n+1))))
+    stra += f"{i//n + 1} + {i%n + 1} = {(i//n+1) + (i%n+1)}" + " "*(((n+n)//10+1)-(i//n+1+i%n+1)//10)
+    #(expect_longest - len(str((i//n+1) + (i%n+1))))
     if i % n == n - 1:
         print(stra)
         stra = ""
--- hw3-2fr.py
+++ hw3-2fr.py
@@ -4,6 +4,6 @@ print("-" * 55)
 expect_longest = len(str(n + n)) + 1
 for i in range(1, n+1):#loop 1
     for j in range(1, n+1): #loop 2
-        print("%d + %d = %d" % (i, j, i+j), end=(" "*(expect_longest-len(str(i+j))))) #print the result
+        print("%d + %d = %d" % (i, j, i+j), end=(" "*(((n+n)//10+1)-(i+j)//10))) #print the result
     print()
 print("-" * 55)
