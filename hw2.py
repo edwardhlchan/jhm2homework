1582334de8cc9d03dac9df409e6ee2e0d6f8cf7e
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2025-02-15 10:21:21 +0800

    fix 2 ?

--- hw2.py
+++ hw2.py
@@ -6,10 +6,20 @@ def part2():
         'cookies': "Can you share with your friends?",
         'candies': "Don’t eat too much."
     }
-    print(dt.get(b, ""))
+    if b in dt:
+        print(dt[b])
+        return
+    else:
+        print("Invalid Choice.")
+        part2()
 
-a = input("Do you want some snacks? (yes/no)")
-if a == 'no':
-    print("Good! Let’s play games instead.")
-elif a == 'yes':
-    part2()
+while True:
+    a = input("Do you want some snacks? (yes/no)")
+    if a == 'no':
+        print("Good! Let’s play games instead.")
+        break
+    elif a == 'yes':
+        part2()
+    else:
+        print("Please enter yes or no.")
+        continue
